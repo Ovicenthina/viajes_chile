cfd4c4d5d0d76b2552f092b6b11ac71ca92b075d
Orihana Triana <ovicenthina25@gmail.com>
2021-12-03 23:34:40 -0300

    primer commit del proyecto

--- Apoyo prueba/assets/js/script.js
+++ Apoyo prueba/assets/js/script.js
@@ -1,1 +1,25 @@
+$(document).ready(function(){
+   
+    $("a").click(function(){
+       var numeral= this.hash 
+
+       $("html, body").anime(
+           {scrollTop:$(numeral).offset().top -70
+       },
+       800
+       )
+
+    })
+})
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
 
